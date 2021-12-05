c50b32da5ba31f6a87b08badc7b66cb2c2302554
Stoyan Georgiev <stoyan.georgiev95@gmail.com>
2021-12-05 20:38:44 +0100

    day5.p1.2021 done

---
+++ 2021/day 5 hydrothermal vents/overlapping_wires.py
@@ -0,0 +1,153 @@
+"""
+--- Day 5: Hydrothermal Venture ---
+You come across a field of hydrothermal vents on the ocean floor! These vents constantly produce large, opaque clouds, so it would be best to avoid them if possible.
+
+They tend to form in lines; the submarine helpfully produces a list of nearby lines of vents (your puzzle input) for you to review. For example:
+
+0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+Each line of vents is given as a line segment in the format x1,y1 -> x2,y2 where x1,y1 are the coordinates of one end the line segment and x2,y2 are the coordinates of the other end. These line segments include the points at both ends. In other words:
+
+An entry like 1,1 -> 1,3 covers points 1,1, 1,2, and 1,3.
+An entry like 9,7 -> 7,7 covers points 9,7, 8,7, and 7,7.
+For now, only consider horizontal and vertical lines: lines where either x1 = x2 or y1 = y2.
+
+So, the horizontal and vertical lines from the above list would produce the following diagram:
+
+.......1..
+..1....1..
+..1....1..
+.......1..
+.112111211
+..........
+..........
+..........
+..........
+222111....
+In this diagram, the top left corner is 0,0 and the bottom right corner is 9,9. Each position is shown as the number of lines which cover that point or . if no line covers that point. The top-left pair of 1s, for example, comes from 2,2 -> 2,1; the very bottom row is formed by the overlapping lines 0,9 -> 5,9 and 0,9 -> 2,9.
+
+To avoid the most dangerous areas, you need to determine the number of points where at least two lines overlap. In the above example, this is anywhere in the diagram with a 2 or larger - a total of 5 points.
+
+Consider only horizontal and vertical lines. At how many points do at least two lines overlap?
+"""
+from collections import namedtuple, Counter
+import os
+from typing import List
+
+Point = namedtuple('Point', ['x','y'])
+
+RAW_INPUT = """0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+"""
+
+cd = os.path.abspath(os.getcwd())
+
+def all_points_lists(file_path: str)-> List[List[Point]]:
+    all_points = list()
+    with open(file_path) as f:
+        for line in f:
+            
+            line = line.strip()
+            arraw_inx = line.index('->')
+            x1, y1 = line[:arraw_inx-1].split(',')
+            x2, y2 = line[arraw_inx+3:].split(',')
+            start_p = Point(int(x1), int(y1))
+            end_p = Point(int(x2), int(y2))
+            new_points = list()
+            if start_p.x == end_p.x:
+                if start_p.y < end_p.y:
+                    diff = end_p.y - start_p.y
+                    for i in range(start_p.y, start_p.y+diff+1): 
+                        new_points.append(Point(start_p.x, i))
+                    all_points.append(new_points)
+                    #new_points.clear()
+                elif start_p.y > end_p.y:
+                    diff = start_p.y - end_p.y
+                    for i in range(start_p.y, start_p.y-diff-1, -1):
+                        new_points.append(Point(start_p.x, i))
+                    all_points.append(new_points)
+                    #new_points.clear()
+            elif start_p.y == end_p.y:
+                if start_p.x > end_p.x:
+                    diff = start_p.x - end_p.x
+                    for i in range(start_p.x, start_p.x-diff-1, -1):
+                        new_points.append(Point(i, start_p.y))
+                    all_points.append(new_points)
+                elif start_p.x < end_p.x:
+                    diff = end_p.x - start_p.x
+                    for i in range(start_p.x,start_p.x+diff+1):
+                        new_points.append(Point(i, start_p.y))
+                    all_points.append(new_points)
+                    #new_points.clear()
+            else:
+                continue
+        
+    return all_points
+
+def count_occurences(all_points: List[List[Point]])-> int:
+    all_counts = list()
+    total = Counter()
+    count_occurs = 0
+
+    for line in all_points:
+        print(line)
+        all_counts.append(Counter(line))
+    #print(all_counts)
+    for count in all_counts:
+        total += count
+    
+    #print(total)
+    for t in total:
+        if total[t] >= 2:
+            count_occurs += 1
+    #print(count_occurs)
+    return count_occurs
+
+PATH = f'{cd}/input.txt'
+all_pts = all_points_lists(PATH)
+
+ctr = count_occurences(all_pts)
+print(ctr)
+#if ctr != False:
+    #print(ctr)
+
+"""
+--- Part Two ---
+Unfortunately, considering only horizontal and vertical lines doesn't give you the full picture; you need to also consider diagonal lines.
+
+Because of the limits of the hydrothermal vent mapping system, the lines in your list will only ever be horizontal, vertical, or a diagonal line at exactly 45 degrees. In other words:
+
+An entry like 1,1 -> 3,3 covers points 1,1, 2,2, and 3,3.
+An entry like 9,7 -> 7,9 covers points 9,7, 8,8, and 7,9.
+Considering all lines from the above example would now produce the following diagram:
+
+1.1....11.
+.111...2..
+..2.1.111.
+...1.2.2..
+.112313211
+...1.2....
+..1...1...
+.1.....1..
+1.......1.
+222111....
+You still need to determine the number of points where at least two lines overlap. In the above example, this is still anywhere in the diagram with a 2 or larger - now a total of 12 points.
+
+Consider all of the lines. At how many points do at least two lines overlap?
+"""
--- README.md
+++ README.md
@@ -6,9 +6,11 @@ D1: Both, D2: Both </br>
 D1: Both, D2: Both </br>
 
 # 2019's Advent of Code
-D1: Both, D2: Both </br>
+D1: Both, D2: Both, D3:Both* </br>
+
+*joel
 
 # 2021's Advent of Code
-D1: Both, D2: Both, D3: Both, D4: Both*, </br>
+D1: Both, D2: Both, D3: Both, D4: Both*, D5: √ X </br>
 
-*code for the first part was really ugly, could not manage to solve the second. got help from Ilia</br>
+*code for the first part was really ugly, but worked, could not manage to solve the second. got help from Ilia</br>
