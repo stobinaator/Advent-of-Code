bb581c77374bcf85834c2143c305877d205c45a6
Stoyan Georgiev <stoyan.georgiev95@gmail.com>
2021-12-18 19:14:07 +0100

    day15. 2021

---
+++ 2021/day_15 chiton/risk_level.py
@@ -0,0 +1,53 @@
+"""
+--- Day 15: Chiton ---
+You've almost reached the exit of the cave, but the walls are getting closer together. Your submarine can barely still fit, though; the main problem is that the walls of the cave are covered in chitons, and it would be best not to bump any of them.
+
+The cavern is large, but has a very low ceiling, restricting your motion to two dimensions. The shape of the cavern resembles a square; a quick scan of chiton density produces a map of risk level throughout the cave (your puzzle input). For example:
+
+1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581
+You start in the top left position, your destination is the bottom right position, and you cannot move diagonally. The number at each position is its risk level; to determine the total risk of an entire path, add up the risk levels of each position you enter (that is, don't count the risk level of your starting position unless you enter it; leaving it adds no risk to your total).
+
+Your goal is to find a path with the lowest total risk. In this example, a path with the lowest total risk is highlighted here:
+
+1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581
+The total risk of this path is 40 (the starting position is never entered, so its risk is not counted).
+
+What is the lowest total risk of any path from the top left to the bottom right?
+523
+
+--- Part Two ---
+Now that you know how to find low-risk paths in the cave, you can try to find your way out.
+
+The entire cave is actually five times larger in both dimensions than you thought; the area you originally scanned is just one tile in a 5x5 tile area that forms the full map. Your original map tile repeats to the right and downward; each time the tile repeats to the right or downward, all of its risk levels are 1 higher than the tile immediately up or left of it. However, risk levels above 9 wrap back around to 1. So, if your original map had some position with a risk level of 8, then that same position on each of the 25 total tiles would be as follows:
+
+8 9 1 2 3
+9 1 2 3 4
+1 2 3 4 5
+2 3 4 5 6
+3 4 5 6 7
+Each single digit above corresponds to the example position with a value of 8 on the top-left tile. 
+
+Because the full map is actually five times larger in both dimensions, that position appears a total of 25 times, once in each duplicated tile, with the values shown above.
+The total risk of this path is 315 (the starting position is still never entered, so its risk is not counted).
+
+Using the full map, what is the lowest total risk of any path from the top left to the bottom right?
+2876
+"""
--- README.md
+++ README.md
@@ -38,8 +38,8 @@ Each day's folder holds an input file with test-data, my solution and a Joel Gru
 - [x] D12: Both°7
 - [x] D13: Both°8
 - [x] D14: Both°9
-- [ ] D15: 
-- [x] D16: Both°X
+- [x] D15: Both°X
+- [x] D16: Both°XI
 - [ ] D17:
 
 °1:code for the first part was really ugly, but worked, could not manage to solve the second. got help from Ilia.</br>
@@ -51,4 +51,4 @@ Each day's folder holds an input file with test-data, my solution and a Joel Gru
 °7:no idea how to do it. at all. - help for both.</br>
 °8:own code works with example input. need help for real input.</br>
 °9:got it working for the first part, the second part wanted really big computation and my solution didn't work. found a really good one on reddit.</br>
-°X: joel</br>
+°X&XI: joel</br>
