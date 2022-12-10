b32df79a693596356fc35fa2ff24aca2e8f3ca9b
Stoyan Georgiev <stoyan@pace.car>
2022-12-10 15:55:55 +0100

    refactor

--- 2022/day_9 - Rope Bridge/jonathan_paulson.py
+++ 2022/day_9 - Rope Bridge/jonathan_paulson.py
@@ -18,19 +18,16 @@ H = (0,0)
 T = [(0,0) for _ in range(9)]
 DR = {'L':0, 'U':-1, 'R': 0, 'D': 1}
 DC = {'L':-1, 'U':0, 'R': 1, 'D': 0}
-P1 = set()
-P2 = set()
+P1 = set([T[0]])
+P2 = set([T[8]])
 for line in lines:
     direction, delta = line.split()
     delta = int(delta)
     for _ in range(delta):
-        P1.add(T[0])
-        P2.add(T[8])
         H = (H[0] + DR[direction], H[1] + DC[direction])
         T[0] = adjust(H,T[0])
         for i in range(1,9):
             T[i] = adjust(T[i-1], T[i])
-
         P1.add(T[0])
         P2.add(T[8])
 print(len(P1))
