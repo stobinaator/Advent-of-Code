ad876c4c556a00cf423cef7bd9043f095258488b
Stoyan Georgiev <stoyan@pace.car>
2022-12-22 18:09:59 +0100

    merge both functions into one

--- 2022/day 20 - Grove Positioning System/jonathan_paulson.py
+++ 2022/day 20 - Grove Positioning System/jonathan_paulson.py
@@ -1,34 +1,12 @@
 from collections import deque
 data = open("input.txt").read().strip().splitlines()
 
-def part_1(data):
+def solve(data, part):
     X = [int(x) for x in data]
+    if part == 2:
+        X = [x*811589153 for x in X]
     X = deque(list(enumerate(X)))
-    for i in range(len(X)):
-        for j in range(len(X)):
-            if X[j][0] == i:
-                break
-        while X[0][0] != i:
-            X.append(X.popleft())
-        val = X.popleft()
-        to_pop = val[1]
-        to_pop %= len(X)
-        
-        for _ in range(to_pop):
-            X.append(X.popleft())
-        X.append(val)
-
-    for j in range(len(X)):
-        if X[j][1] == 0:
-            break
-
-    print(X[(j+1000)%len(X)][1] + X[(j+2000)%len(X)][1] + X[(j+3000)%len(X)][1])
-
-def part_2(data):
-    X = [int(x) for x in data]
-    X = [x*811589153 for x in X]
-    X = deque(list(enumerate(X)))
-    for t in range(10):
+    for t in range(10 if part == 2 else 1):
         for i in range(len(X)):
             for j in range(len(X)):
                 if X[j][0] == i:
@@ -49,5 +27,5 @@ def part_2(data):
 
     print(X[(j+1000)%len(X)][1] + X[(j+2000)%len(X)][1] + X[(j+3000)%len(X)][1])
 
-# part_1(data)
-part_2(data)
+solve(data, 1)
+solve(data, 2)
